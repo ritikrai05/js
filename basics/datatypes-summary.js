91239f5fbe481051ebd8b7fbda3a56cba81c27df
ritikrai05 <167898496+ritikrai05@users.noreply.github.com>
2024-04-24 07:21:14 +0000

    datatypes-summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// primitive
+//primitive are of  7 types given below
+// 7 tyes : String,Number,Boolean,Null,Undefined,Symbole,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+//const id =symbol('123')
+//const anotherId=symbol('123')
+
+//console.log(id=== anotherId);
+ 
+
+const bigNumber=7021495043n
+
+
+
+
+
+
+// Reference(Non primitive)
+
+// Arry,Objects,Functions
+const heros=["shaktiman","naagraj","doga"]
+{name:"ritik"}
+
+
+const myfunction= function() {
+
+    console.log("hello world");
+}
+
+console.log( typeof myfunction);
