fa29d8aed06ed83a493f5f2b11e1b03d22d71f4d
ritikrai05 <167898496+ritikrai05@users.noreply.github.com>
2024-04-24 16:03:07 +0000

    string

---
+++ basics/string.js
@@ -0,0 +1,25 @@
+const name="ritik"
+const repoCount =50
+
+console.log(name+repoCount+"value");
+
+console.log(`hello my name is ${name}and my repo count is ${repoCount}`);
+
+
+const gamename= new String('ritikrai');
+
+
+console.log(gamename[0]);
+
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.indexOf('t'));
+console.log(gamename.charAt(2));
+
+const newString = gamename.substring(0,5)
+
+console.log(newString);
+
+
+
