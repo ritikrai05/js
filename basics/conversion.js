ad2b6afa01bd8f619e4fa86bd97d8ceaf9ff0308
ritikrai05 <167898496+ritikrai05@users.noreply.github.com>
2024-04-24 06:27:24 +0000

    conversion2

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,6 +1,27 @@
-let score=33
+let score=undefined
 
  console.log(typeof score);
  console.log(typeof(score));
 
  let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=>33
+//"33abc"=>NaN
+// true=>1;false=>0
+
+let isloggedIn=""
+
+let booleanIsLoggedIn=Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true;0=>false
+//""=>false
+//ritik=>true
+let num=33
+
+let stringnumber=String(num)
+console.log(stringnumber);
+console.log(typeof stringnumber);
