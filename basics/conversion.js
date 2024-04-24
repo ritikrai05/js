6a7ca308beba986f865d13b9d102424a5b21e716
ritikrai05 <167898496+ritikrai05@users.noreply.github.com>
2024-04-24 06:45:01 +0000

    conversion3

--- basics/conversion.js
+++ basics/conversion.js
@@ -25,3 +25,63 @@ let num=33
 let stringnumber=String(num)
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+
+//***************************Operations**************** 
+
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+
+let str1="ritik"
+let sr2="hello"
+
+let str3=str1 + sr2
+
+console.log(str3);
+
+console.log(((3+4)*5)%3);
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
