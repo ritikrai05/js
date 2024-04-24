b2532391f83fa779cae81b2ff6f57ec85d2db45a
ritikrai05 <167898496+ritikrai05@users.noreply.github.com>
2024-04-24 05:38:51 +0000

    datatype ad var

---
+++ basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs,not browser
+
+console.log(3+3); 
+//code readbility should ne high
+console.log("Ritik");
+
+let name="ritik"
+let age=18
+let isloggedIn=false
+
+//number=>2to power 53
+//bigint
+//string=>""
+//boolean>true/false
+//null=>standalone value(it is empty or null)
+//undefined=>
+//symbol=>unique
+
+
+//object
+
+console.log(typeof"ritik");
---
+++ basics/var.js
@@ -0,0 +1,23 @@
+const accountId=144553
+let accountEmail="riti@gmail.com"
+var accountpassword="12345"
+accountCity ="mumbai"
+let accountState;
+
+//accountId =2//not allowed
+
+accountEmail ="rai@gmail.com"
+accountpassword ="5555"
+accountCity ="pune"
+
+
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block  scope and functional scope
+
+*/
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
