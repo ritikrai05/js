2f4f91da7e88a31f07ae5f4076d94a63bba8a24b
ritikrai05 <167898496+ritikrai05@users.noreply.github.com>
2024-04-25 07:20:20 +0000

    date

---
+++ basics/date.js
@@ -0,0 +1,17 @@
+// ++++++++++++++++Date++++++++++++++
+
+let mydate = new Date()
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+
+//let mydate1 = new Date(2024,0,23);
+//let mydate1 = new Date(2024,0,23);
+//let mydate1 = new Date(2024,0,23,5,3);
+let mydate1 = new Date("01-14-2024");
+console.log(mydate1);
+console.log(mydate1.toDateString());
+console.log(mydate1.toLocaleString());
